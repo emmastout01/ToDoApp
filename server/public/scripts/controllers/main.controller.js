e255ca6d20fb856b4e059aa94e4a67e0e343b088
Emma Stout <emma.stout01@gmail.com>
2018-02-18 13:24:55 -0600

    post route on client side

--- server/public/scripts/controllers/main.controller.js
+++ server/public/scripts/controllers/main.controller.js
@@ -4,4 +4,12 @@ myApp.controller('MainController', function($location, MainService) {
     vm.mainService = MainService;
 
 
+    vm.addTask = function(newTask) {
+        vm.mainService.addTask(newTask)
+        // .then(function() {
+        //     vm.mainService.getTasks();
+        // })
+    }
+
+
 }); //end main controller
--- server/public/scripts/services/main.service.js
+++ server/public/scripts/services/main.service.js
@@ -1,7 +1,16 @@
 myApp.service('MainService', function($http) {
     console.log('main service created');
     var self = this;
-    
+
+    self.addTask = function(newTask) {
+       return $http.post('/task', newTask)
+       .then(function(response) {
+            console.log('new task posted', response);
+            return response;
+       }).catch(function(err) {
+           console.log('error with post', err);
+       })
+    }
 
 
 
